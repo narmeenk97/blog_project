bdefc124938cd0edbde1f1c4b33768c9376d6ed4
Narmeen Khalid <144829367+narmeenk97@users.noreply.github.com>
2025-04-10 14:21:26 -0400

    fixed apostrophe2

--- src/app/_components/Intro.tsx
+++ src/app/_components/Intro.tsx
@@ -4,6 +4,7 @@ export default function Intro() {
     return(
         <div className="flex flex-col md:flex-row items-center justify-center text-white px-4 mt-12">
             <div className="mr-4 px-5 py-3 w-1/2">
+            {/* eslint-disable-next-line react/no-unescaped-entities */}
                 <h1 className="text-blue-400">Welcome to Narmeen&apos;s Blog</h1>
                 <p className="text-4xl py-4 font-bold">Learning Tailwind CSS</p>
                 <form className="max-w-sm">
@@ -17,7 +18,7 @@ export default function Intro() {
                 </p>
             </div>
             <div className="px-5 py-3 w-1/2">
-                <h1 className="text-blue-400 px-6 py-2 mb-3 mr-3 mt-4 ">Subscribe to my blog to keep up with the projects I'm working on currently.</h1>
+                <h1 className="text-blue-400 px-6 py-2 mb-3 mr-3 mt-4 ">Subscribe to my blog to keep up with the projects I am working on currently.</h1>
                 <p className="text-blue-200 px-6 py-2 mb-3 mr-3 mt-4 ">This is a purely front-end project for me to develop my skills in Tailwind CSS.</p>
             </div>
         </div>
