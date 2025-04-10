988c888aaa2e69797766fe7436330a4e49dd683c
Narmeen Khalid <144829367+narmeenk97@users.noreply.github.com>
2025-04-10 14:08:32 -0400

    Added Intro component to main page

---
+++ src/app/_components/nav_bar.tsx
@@ -0,0 +1,64 @@
+import Link from "next/link";
+
+export default function NavBar() {
+    return (
+        <header className="text-white">
+          <nav className="flex items-center justify-between px-4 mt-5">
+            <div>
+              <ul className="flex items-center space-x-6">
+            <li>
+              <div className="relative inline-block text-left">
+                  <button type="button" className="inline-flex w-full justify-center gap-x-1.5 rounded-md border-2 border-white font-extrabold px-5 py-3" aria-expanded="true" aria-haspopup="true">
+                    Narmeen
+                  <svg className="-mr-1 size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
+                    <path fill-rule="evenodd" d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
+                  </svg>
+                  </button>
+                </div>
+                </li>
+                <li>
+                <div className="relative inline-block text-left">
+                  <button type="button" className="inline-flex w-full justify-center gap-x-1.5 px-2 pl-2">Products
+                    <svg className="-mr-1 size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
+                      <path fill-rule="evenodd" d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
+                    </svg>
+                  </button>
+                </div>
+                </li>
+                <li>
+                <div className="relative inline-block text-left">
+                  <button type="button" className="inline-flex w-full justify-center gap-x-1.5 px-2 pl-2">Services
+                    <svg className="-mr-1 size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
+                      <path fill-rule="evenodd" d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
+                    </svg>
+                  </button>
+                </div>
+                </li>
+                <li>
+                <Link href="/" className="p-2">Pricing</Link>
+                </li>
+                <li>
+                <div className="relative inline-block text-left">
+                  <button type="button" className="inline-flex w-full justify-center gap-x-1.5 px-2 pl-2">Resources
+                    <svg className="-mr-1 size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
+                      <path fill-rule="evenodd" d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
+                    </svg>
+                  </button>
+                </div>
+                </li>
+                <li>
+                <Link href="/" className="p-2">About</Link>
+                </li>
+            </ul>
+            </div>
+              <div className="flex items-center space-x-5">
+                <button type="button" className="px-5 py-3 bg-blue-900 border-1 border-blue-600 rounded-md">Login</button>
+                <button type="button" className="px-5 py-3 bg-blue-950 border-1 border-blue-700 rounded-md">Sign Up</button>
+              </div>
+          </nav>
+          <div className="flex lg:hidden justify-end p-4">
+            <i className="fa-solid fa-bars"></i>
+          </div>
+        </header>
+    );
+  }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,19 @@
-import Link from "next/link";
+import React from "react";
+import Navbar from "./_components/nav_bar";
+import Intro from "./_components/Intro";
 
 export default function Home() {
   return (
-    <body>
-      <header className="text-white">
-        <nav className="flex items-center justify-evenly space-x-6 mt-5">
-          <div className="flex items-center justify-evenly">
-            <ul>
-              <Link href="/" className="border-2 border-white font-extrabold px-5 py-3">Narmeen</Link>
-              <Link href="/" className="p-2">Products</Link>
-              <Link href="/" className="p-2">Services</Link>
-              <Link href="/" className="p-2">Pricing</Link>
-              <Link href="/" className="p-2">Resources</Link>
-              <Link href="/" className="p-2">About</Link>
-            </ul>
-            <div className="flex justify-end space-x-5">
-              <button className="px-5 py-3 bg-blue-900 border-1 border-blue-600 rounded-md">Login</button>
-              <button className="px-5 py-3 bg-blue-950 border-1 border-blue-700 rounded-md">Sign Up</button>
-            </div>
-          </div>
-          <div>
-            <i className="fa-solid fa-bars"></i>
-          </div>
-        </nav>
-      </header>
-    </body>
+    <div>
+      {/* Header and Navbar */}
+    <header>
+      <Navbar />
+    </header>
+    <main>
+      <div>
+        <Intro></Intro>
+      </div>
+    </main>
+  </div>
   );
 }
